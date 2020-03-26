3d2d3bba920be8d1be25a1ee9c008800fd020537
Ciprian Dorin Craciun <ciprian@volution.ro>
2020-03-26 17:30:48 +0200

    [sources]  Add support for normalized record values;  add support for TSV parsing.

--- sources/csv-to-json.py
+++ sources/csv-to-json.py
@@ -9,12 +9,17 @@ import unidecode
 
 
 
-def convert (_input, _output, _dataset) :
+def convert (_input, _output, _dataset, _format) :
 	
 	_input = file (_input, "r")
 	_output = file (_output, "w+")
 	
-	_reader = csv.DictReader (_input, dialect = "excel")
+	if _format == "csv" :
+		_reader = csv.DictReader (_input, dialect = "excel")
+	elif _format == "tsv" :
+		_reader = csv.DictReader (_input, dialect = "excel-tab")
+	else :
+		raise Exception (("[c9145e78]", _format))
 	
 	_records = []
 	_keys = {}
@@ -27,8 +32,11 @@ def convert (_input, _output, _dataset) :
 					_keys[_key_normalized] = []
 				_keys[_key_normalized].append (_key)
 				_keys_reverse[_key] = _key_normalized
-		_record = {_keys_reverse[_key] : _normalize_value (_value) for _key, _value in _record.iteritems ()}
-		_records.append (_record)
+		_record_0 = {}
+		for _key, _value in _record.iteritems () :
+			_record_0[_keys_reverse[_key]] = _normalize_value (_value)
+			_record_0[_keys_reverse[_key] + "__normalized"] = _normalize_key_0 (_value, False)
+		_records.append (_record_0)
 	
 	_data = {
 			"dataset" : _dataset,
@@ -54,19 +62,33 @@ def _normalize_key (_key) :
 	if _key in _normalize_key_cache :
 		return _normalize_key_cache[_key]
 	
+	_normalized = _normalize_key_0 (_key, True)
+	
+	_normalize_key_cache[_key] = _normalized
+	
+	return _normalized
+
+_normalize_key_cache = dict ()
+
+
+def _normalize_key_0 (_key, _is_key) :
+	
 	_key = _key.decode ("utf-8")
 	
 	_normalized = unidecode.unidecode (_key)
 	_normalized = _normalized.lower ()
 	_normalized = _normalize_key_forbidden.sub ("_", _normalized)
 	_normalized = _normalized.strip ("_")
-	if _normalized[0] >= "0" and _normalized[0] <= "9" :
-		_normalized = "_" + _normalized
 	
-	_normalize_key_cache[_key] = _normalized
+	if _normalized == "" :
+		return None
+	
+	if _normalized[0] >= "0" and _normalized[0] <= "9" :
+		if _is_key :
+			_normalized = "_" + _normalized
+	
 	return _normalized
 
-_normalize_key_cache = dict ()
 _normalize_key_forbidden = re.compile ("[^A-Za-z0-9_]+")
 
 
@@ -76,6 +98,8 @@ def _normalize_value (_value) :
 	
 	if _value == "" :
 		return None
+	
+	_value = _value.decode ("utf-8")
 	
 	_value = _value.strip (" ")
 	_value = _value.strip ("\t")
@@ -97,7 +121,6 @@ def _normalize_value (_value) :
 	except :
 		pass
 	
-	_value = _value.decode ("utf-8")
 	_value = unidecode.unidecode (_value)
 	
 	return _value
